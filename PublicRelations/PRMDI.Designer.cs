315c8708edc38de6f98532bca8db38832258d8e5
ToolsForInternetInfluencer <129713063+ToolsForInternetInfluencer@users.noreply.github.com>
2023-04-04 21:49:35 -0600

    Initial commit
    Initial commit

---
+++ PublicRelations/PRMDI.Designer.cs
@@ -0,0 +1,178 @@
+﻿namespace PublicRelations
+{
+    partial class PRMDI
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.socialMediaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.youtubeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.faceBookToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.redditToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.weChatToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.pinTestToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.messangerToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.whatsAppToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.telegramToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.tikTokToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.discordToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.snapchatToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.socialMediaToolStripMenuItem,
+            this.messangerToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 30);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // socialMediaToolStripMenuItem
+            // 
+            this.socialMediaToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.youtubeToolStripMenuItem,
+            this.faceBookToolStripMenuItem,
+            this.redditToolStripMenuItem,
+            this.weChatToolStripMenuItem,
+            this.pinTestToolStripMenuItem});
+            this.socialMediaToolStripMenuItem.Name = "socialMediaToolStripMenuItem";
+            this.socialMediaToolStripMenuItem.Size = new System.Drawing.Size(109, 26);
+            this.socialMediaToolStripMenuItem.Text = "Social Media";
+            // 
+            // youtubeToolStripMenuItem
+            // 
+            this.youtubeToolStripMenuItem.Name = "youtubeToolStripMenuItem";
+            this.youtubeToolStripMenuItem.Size = new System.Drawing.Size(155, 26);
+            this.youtubeToolStripMenuItem.Text = "Youtube";
+            // 
+            // faceBookToolStripMenuItem
+            // 
+            this.faceBookToolStripMenuItem.Name = "faceBookToolStripMenuItem";
+            this.faceBookToolStripMenuItem.Size = new System.Drawing.Size(155, 26);
+            this.faceBookToolStripMenuItem.Text = "FaceBook";
+            // 
+            // redditToolStripMenuItem
+            // 
+            this.redditToolStripMenuItem.Name = "redditToolStripMenuItem";
+            this.redditToolStripMenuItem.Size = new System.Drawing.Size(155, 26);
+            this.redditToolStripMenuItem.Text = "Reddit";
+            // 
+            // weChatToolStripMenuItem
+            // 
+            this.weChatToolStripMenuItem.Name = "weChatToolStripMenuItem";
+            this.weChatToolStripMenuItem.Size = new System.Drawing.Size(155, 26);
+            this.weChatToolStripMenuItem.Text = "WeChat";
+            // 
+            // pinTestToolStripMenuItem
+            // 
+            this.pinTestToolStripMenuItem.Name = "pinTestToolStripMenuItem";
+            this.pinTestToolStripMenuItem.Size = new System.Drawing.Size(155, 26);
+            this.pinTestToolStripMenuItem.Text = "PinTest";
+            // 
+            // messangerToolStripMenuItem
+            // 
+            this.messangerToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.whatsAppToolStripMenuItem,
+            this.telegramToolStripMenuItem,
+            this.tikTokToolStripMenuItem,
+            this.discordToolStripMenuItem,
+            this.snapchatToolStripMenuItem});
+            this.messangerToolStripMenuItem.Name = "messangerToolStripMenuItem";
+            this.messangerToolStripMenuItem.Size = new System.Drawing.Size(94, 26);
+            this.messangerToolStripMenuItem.Text = "Messenger";
+            // 
+            // whatsAppToolStripMenuItem
+            // 
+            this.whatsAppToolStripMenuItem.Name = "whatsAppToolStripMenuItem";
+            this.whatsAppToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.whatsAppToolStripMenuItem.Text = "Whats App";
+            // 
+            // telegramToolStripMenuItem
+            // 
+            this.telegramToolStripMenuItem.Name = "telegramToolStripMenuItem";
+            this.telegramToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.telegramToolStripMenuItem.Text = "Telegram";
+            // 
+            // tikTokToolStripMenuItem
+            // 
+            this.tikTokToolStripMenuItem.Name = "tikTokToolStripMenuItem";
+            this.tikTokToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.tikTokToolStripMenuItem.Text = "TikTok";
+            // 
+            // discordToolStripMenuItem
+            // 
+            this.discordToolStripMenuItem.Name = "discordToolStripMenuItem";
+            this.discordToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.discordToolStripMenuItem.Text = "Discord";
+            // 
+            // snapchatToolStripMenuItem
+            // 
+            this.snapchatToolStripMenuItem.Name = "snapchatToolStripMenuItem";
+            this.snapchatToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.snapchatToolStripMenuItem.Text = "Snapchat";
+            // 
+            // PRMDI
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.menuStrip1);
+            this.IsMdiContainer = true;
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "PRMDI";
+            this.Text = "Public Relations MDI";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem socialMediaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem youtubeToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem faceBookToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem redditToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem messangerToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem whatsAppToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem telegramToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem tikTokToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem discordToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem snapchatToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem weChatToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem pinTestToolStripMenuItem;
+    }
+}
+
